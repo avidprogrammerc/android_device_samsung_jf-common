0d86e33de198b9f0fe3c60087974cbfc20e2c0d1
Chris Conley <chrisc93@vt.edu>
2013-08-08 13:14:33 -0400

    Revert "Irda: Enable Irda service via overlay and HAL"
    This reverts commit 19d7f770c1a15391335f07b06b5ef5e9ecab4c5b.
    
    Signed-off-by: Chris Conley <chrisc93@vt.edu>
    
    Conflicts:
    	jf-common.mk
    
    Change-Id: Ifb75cb4f7ae64cfee24e9ec837e70e639dc2a18a

--- jf-common.mk
+++ jf-common.mk
@@ -105,9 +105,6 @@ PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
 # Lights
 PRODUCT_PACKAGES += lights.msm8960
 
-# Irda
-PRODUCT_PACKAGES += irda.msm8960
-
 # QC Perf
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.vendor.extension_library=/system/lib/libqc-opt.so
